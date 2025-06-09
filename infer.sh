cb3e5951732c4691362bcbf04c316bccb214ae97
hqbbzsp <140230694+hqbbzsp@users.noreply.github.com>
2025-06-09 20:13:22 +0800

    Update infer.sh

--- infer.sh
+++ infer.sh
@@ -1,4 +1,4 @@
-FPS_MAX_FRAMES=90 VIDEO_MAX_PIXELS=$((400*28*28)) VIDEO_TOTAL_PIXELS=$((90 *400*28*28)) FPS=1.5 \
+FPS_MAX_FRAMES=120 VIDEO_MAX_PIXELS=$((400*28*28)) VIDEO_TOTAL_PIXELS=$((120 *400*28*28)) FPS=2 \
 CUDA_VISIBLE_DEVICES=2,3,5 python test.py \
   --model_path ./ModelWeight/Qwen/Qwen2.5-VL-72B-Instruct-AWQ \
   --input_json ./CVPR25_VideoVLM/Dataset/test_data/test.json \
