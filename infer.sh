a27bd557c5757da3cfe996c4d37118b62d8055b1
hqbbzsp <140230694+hqbbzsp@users.noreply.github.com>
2025-06-09 20:13:40 +0800

    Update infer.sh

--- infer.sh
+++ infer.sh
@@ -2,6 +2,6 @@ FPS_MAX_FRAMES=120 VIDEO_MAX_PIXELS=$((400*28*28)) VIDEO_TOTAL_PIXELS=$((120 *40
 CUDA_VISIBLE_DEVICES=2,3,5 python test.py \
   --model_path ./ModelWeight/Qwen/Qwen2.5-VL-72B-Instruct-AWQ \
   --input_json ./CVPR25_VideoVLM/Dataset/test_data/test.json \
-  --output_json ./CVPR25_VideoVLM/test_result/Qwen2.5-VL-72B-Instruct-AWQ-RAW/result_90frame.json \
+  --output_json ./CVPR25_VideoVLM/test_result/Qwen2.5-VL-72B-Instruct-AWQ-RAW/result.json \
   --video_root ./CVPR25_VideoVLM/Dataset/test_data/test-videos \
-> "./CVPR25_VideoVLM/test_result/Qwen2.5-VL-72B-Instruct-AWQ-RAW/log_90frame.txt"
+> "./CVPR25_VideoVLM/test_result/Qwen2.5-VL-72B-Instruct-AWQ-RAW/log.txt"
